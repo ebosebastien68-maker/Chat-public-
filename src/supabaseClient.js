371970d9f42d3b80acb354193f3230dd2f2a2a53
ebosebastien68-maker <ebosebastien68@gmail.com>
2025-09-21 18:13:47 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+// Le code cherche les variables ici, via "import.meta.env"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY // <-- REGARDEZ CE NOM !
 
-export default supabase;
+export const supabase = createClient(supabaseUrl, supabaseKey)
