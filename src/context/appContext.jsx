2b5b1b6628faf5664384a4c9ea33d1bca58b00ad
ebosebastien68-maker <ebosebastien68@gmail.com>
2025-09-22 05:17:06 +0100

    Update appContext.jsx

--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useRef, useState } from "react";
-import supabase from "../supabaseClient";
+import supabase from "../supabaseClient.js";
 
 const AppContext = createContext({});
 
